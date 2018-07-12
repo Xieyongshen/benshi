6d859ec52ed2578f2e7c2bf3a1d77bdecc49efb8
Aini <1310178049@qq.com>
2018-07-12 17:27:10 +0800

    add the authorizen with local server

--- myproject/src/api/api.js
+++ myproject/src/api/api.js
@@ -2,8 +2,11 @@ import {
     wxRequest
 } from '@/utils/wxRequest';
 
-const apiBenshi = ''
+const apiBenshi = 'http://127.0.0.1:8000'
 
 //微信的jscode换取sessionKey
 const wxJsCode2Session = (params) => wxRequest(params, apiBenshi + "/api/wechat/jscode2session");
 const user2session = (params) => wxRequest(params, apiBenshi + "/api/wechat/user2session?jsoncallback=?");
+
+//测试能否连接本地服务器
+const testCode = (params) => wxRequest(params, apiBenshi + "/new_add");
