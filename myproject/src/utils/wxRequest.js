384b7f5482366fdee5c2f182297800b9bef7d8fe
Aini <1310178049@qq.com>
2018-07-16 11:26:39 +0800

    Add the packaged api request

--- myproject/src/utils/wxRequest.js
+++ myproject/src/utils/wxRequest.js
@@ -7,17 +7,35 @@ const API_SECRET_KEY = 'www.benshi.com'
 const TIMESTAMP = util.getCurrentTime()
 const SIGN = md5.hex_md5((TIMESTAMP + API_SECRET_KEY).toLowerCase())
 
+// const wxRequest = async(params = {}, url) => {
+//     tip.loading();
+//     let data = params.query || {};
+//     data.sign = SIGN;
+//     data.time = TIMESTAMP;
+//     let res = await wepy.request({
+//         url: url,
+//         method: params.method || 'GET',
+//         data: data,
+//         header: { 'Content-Type': 'application/json' },
+//     });
+//     tip.loaded();
+//     return res;
+// };
+
 const wxRequest = async(params = {}, url) => {
     tip.loading();
     let data = params.query || {};
-    data.sign = SIGN;
-    data.time = TIMESTAMP;
     let res = await wepy.request({
         url: url,
         method: params.method || 'GET',
         data: data,
         header: { 'Content-Type': 'application/json' },
+        // success: function(res1) {
+        //     // console.log(res1)
+        //     return res1;
+        // }
     });
+    // console.log(res)
     tip.loaded();
     return res;
 };
