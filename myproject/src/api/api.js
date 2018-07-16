da8215f5e49027238fa9715102b3a0031c25b0a3
Aini <1310178049@qq.com>
2018-07-16 16:07:47 +0800

    add request in index

--- myproject/src/api/api.js
+++ myproject/src/api/api.js
@@ -10,7 +10,10 @@ const testCode = (params) => wxRequest(params, apiBenshi + "/new_add");
 
 //获取分类页面的大分类和小分类标签
 const getCategory = (params) => wxRequest(params, apiBenshi + "/api/get_category");
+//获取首页推荐标签
+const getIndex = (params) => wxRequest(params, apiBenshi + "/api/get_index/recommand");
 
 export default {
-    getCategory
+    getCategory,
+    getIndex
 }
